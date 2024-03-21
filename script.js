eb531ae9fa574741d96b9fecbe585a3506a02b54
KevDCS <kcruzs@outlook.com>
2024-03-21 17:15:21 +0100

    Cambiada la clase para la rotación de la carte en el style.css, y agregada la función para que la carta gire al hacer click.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-var section = document.getElementById("shuffle-cards");
+// Revolver imagen en cada refresh del browser
 
-for (var i = section.children.length; i >= 0; i--) {
-	section.appendChild(section.children[(Math.random() * i) | 0]);
-}
+// var section = document.getElementById("shuffle-cards");
+
+// for (var i = section.children.length; i >= 0; i--) {
+// 	section.appendChild(section.children[(Math.random() * i) | 0]);
+// }
+
+// Rotar la imagen al hacerle click
+
+const Cards = [...document.querySelectorAll(".card")];
+
+Cards.forEach((card) => {
+	card.addEventListener("click", () => {
+		card.classList.toggle("selected");
+	});
+});
+
+console.log(Cards);
